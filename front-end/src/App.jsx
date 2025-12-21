545b9334d4a0f99dfe20b3871862c3779bcf225e
Franciele Santos Silva <franciellysantos876@gmail.com>
2025-12-21 00:30:56 -0300

    feat(Hearder):Componente finalizado

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,7 @@
+import Header from "./components/Header";
+
 function App() {
-  return (
-  <div className="text-3xl font-bold text-pink-500 underline">App</div>
-  )
-}            
-  
-export default App
-                 
+  return <Header />;
+}
+
+export default App;
