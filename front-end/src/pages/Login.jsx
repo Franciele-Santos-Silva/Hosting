d8d87a19b4c4c135a6c5ef7b6829554e31411fb0
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-01-01 00:04:21 -0300

    style: organizar importações

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
-import axios from "axios";
+import axios from "axi";
 
 const Login = ({ user, setUser }) => {
   const [email, setEmail] = useState("");
