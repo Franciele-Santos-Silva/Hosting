26d77c0a01ac91468363a2fe6452521e0090d6a0
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-01-01 23:10:32 -0300

    style: remove linhas em branco extras no código

--- back-end/index.js
+++ back-end/index.js
@@ -9,10 +9,13 @@ const { PORT } = process.env;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors( {
-  origin:'http://localhost:5173',
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/users", UserRoutes);
 
 app.listen(PORT, () => {
