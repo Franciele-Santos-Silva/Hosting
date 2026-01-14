7c9b30f630d05d6913c945a29cecdb914d6855c7
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-01-14 09:01:37 -0300

    fiz(imageDownloader): arruma erro de baixar foto com o mime-types

--- back-end/ultils/imageDownloader.js
+++ back-end/ultils/imageDownloader.js
@@ -1,33 +1,25 @@
 import download from "image-downloader";
 import mime from "mime-types";
-import path from "node:path";
 
 export const downloadImage = async (link, destination) => {
   const mimeType = mime.lookup(link);
-  if (!mimeType) {
-    throw new Error("Tipo MIME inválido ou não reconhecido.");
-  }
-
-  const extension = mime.extension(mimeType);
-  if (!extension) {
-    throw new Error("Extensão inválida para o tipo MIME.");
-  }
+  const contentType = mime.contentType(mimeType);
+  const extension = mime.extension(contentType);
 
   const filename = `${Date.now()}.${extension}`;
+
+  console.log({mime, contentType, link, extension});
 
   try {
     const options = {
       url: link,
-      dest: path.join(destination, filename),
+      dest: `${destination}${filename}`,
     };
 
     await download.image(options);
 
     console.log("Saved to", filename);
-
-    return filename;
   } catch (error) {
-    console.error("Erro ao baixar a imagem:", error);
-    throw error;
+    console.error(error);
   }
 };
