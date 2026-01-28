115fdabe5af48e20f5cc9848705e49fcac9500da
Franciele Santos Silva <franciellysantos876@gmail.com>
2026-01-27 22:35:58 -0300

    chore(backend): remove dirname.js

--- back-end/ultils/dirname.js
+++
@@ -1,7 +0,0 @@
-import { fileURLToPath } from "url";
-import {dirname} from "node:path";
-
-export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = dirname(__filename);
-
-console.log(__filename, __dirname);
